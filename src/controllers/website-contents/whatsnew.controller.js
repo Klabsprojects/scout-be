39e260ad6dfed008b2e494f683111e94ce7785b9
suthana-klabs <suthana@klabsindia.com>
2024-12-04 11:40:10 +0530

    add whatsnew issue resolved

--- src/controllers/website-contents/whatsnew.controller.js
+++ src/controllers/website-contents/whatsnew.controller.js
@@ -61,10 +61,6 @@ exports.addWhatsNewold = async (req, res) => {
     exports.addWhatsNew = async (req, res) => {
         try {
             console.log('Received request body:', req.body);
-            //console.log('Type of dateOfUpload:', typeof req.body.dateOfUpload);
-            //const parsedDate = new Date(req.body.dateOfUpload);  
-            //req.body.dateOfUpload = parsedDate;
-            //req.body.dateOfUpload = parsedDate instanceof Date && !isNaN(parsedDate.getTime()) ? parsedDate : new Date();
             let query = {};
             query.body = req.body;
     
@@ -77,65 +73,20 @@ exports.addWhatsNewold = async (req, res) => {
                 query.body.fileEnglish = req.files['fileEnglish'][0].path;
                 console.log('Uploaded fileEnglish file path:', req.files['fileEnglish'][0].path);
             }
-
-            // if (req.body.dateOfUpload) {
-            //     const parsedDate = new Date(req.body.dateOfUpload);  // Convert to Date object
-            
-            //     if (isNaN(parsedDate.getTime())) {
-            //         console.log('Invalid date format:', req.body.dateOfUpload);
-            //         return errorRes(res, 'Invalid date format', 'Invalid date format for dateOfUpload');
-            //     }
-            
-            //     query.body.dateOfUpload = parsedDate;
-            //     console.log('Converted dateOfUpload:', typeof query.body.dateOfUpload);
-            // } else {
-            //     console.log('dateOfUpload is missing!');
-            //     return errorRes(res, 'Missing dateOfUpload', 'dateOfUpload field is required');
-            // }
-            
-            // if (req.body.dateOfUpload) {
-            //     const parsedDate = new Date(req.body.dateOfUpload);  // Convert to Date object
-            
-            //     if (isNaN(parsedDate.getTime())) {
-            //         console.log('Invalid date format:', req.body.dateOfUpload);
-            //         return errorRes(res, 'Invalid date format', 'Invalid date format for dateOfUpload');
-            //     }
-            
+            if(req.body.dateOfUpload)
+            {
+                const parsedDate = new Date(req.body.dateOfUpload);
+                query.body.dateOfUpload = parsedDate;
+            }
                 
-            //     console.log('Converted dateOfUpload:', query.body.dateOfUpload);
-            // } else {
-            //     console.log('dateOfUpload is missing!');
-            //     return errorRes(res, 'Missing dateOfUpload', 'dateOfUpload field is required');
-            // }
-            
-           // query.body.dateOfUpload = new Date('2024-01-01T00:00:00.000Z');
-
-           const parsedDate = new Date(req.body.dateOfUpload);
-        query.body.dateOfUpload = parsedDate;
-
-
-            // Ensure dateOfUpload is correctly parsed as a Date object
-            // if (req.body.dateOfUpload) {
-            //     const parsedDate = new Date(req.body.dateOfUpload);  // Convert to Date object
-            //     if (isNaN(parsedDate.getTime())) {
-            //         // If date is invalid, log and handle accordingly
-            //         console.log('Invalid date format:', req.body.dateOfUpload);
-            //         return errorRes(res, 'Invalid date format', 'Invalid date format for dateOfUpload');
-            //     }
-            //     query.body.dateOfUpload = parsedDate;  // Set the field as a Date object
-            //     console.log('Converted dateOfUpload:', query.body.dateOfUpload);
-            // } else {
-            //     console.log('dateOfUpload is missing!');
-            //     return errorRes(res, 'Missing dateOfUpload', 'dateOfUpload field is required');
-            // }
     
             console.log('Final data to be inserted:', req.body);
             
     
             // Insert the data into the database
-            //const results = await commonService.insertOne(db.whatsnew, query);
-            const login = new db.whatsnew(query);
-            let results = await login.save();
+            const results = await commonService.insertOne(db.whatsnew, query.body);
+            // const login = new db.whatsnew(query);
+            // let results = await login.save();
             console.log('results ', results);
             successRes(res, results, SUCCESS.CREATED);
         } catch (error) {
